93479267f4f898c77d8f7dbd8da6f2df1574fc13
amiiiiee <amiameen507@gmail.com>
2025-01-31 17:29:14 +0530

    edits

--- src/components/SlateEditor.jsx
+++ src/components/SlateEditor.jsx
@@ -5,18 +5,3 @@ import { Slate, Editable, withReact } from 'slate-react';
 const SlateEditor = () => {
     const [value, setValue] = useState([
         {
-          type: 'paragraph',
-          children: [{ text: 'Type something here...' }],
-        },
-      ]);
-
-  const editor = useMemo(() => withReact(createEditor()), []);
-
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case 'heading':
-        return <h2 {...props.attributes}>{props.children}</h2>;
-      default:
-        return <p {...props.attributes}>{props.children}</p>;
-    }
-  }, []);
