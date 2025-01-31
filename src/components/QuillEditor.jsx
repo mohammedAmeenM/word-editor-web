a0115320677621ffb2807bb3e94395d3b26cce1f
amiiiiee <amiameen507@gmail.com>
2025-01-31 17:30:10 +0530

    edits

--- src/components/QuillEditor.jsx
+++ src/components/QuillEditor.jsx
@@ -5,20 +5,3 @@ import 'quill/dist/quill.snow.css'; // Import Quill's Snow theme CSS
 const QuillEditor = () => {
   const quillRef = useRef(null);
   const [editorContent, setEditorContent] = useState('');
-
-  useEffect(() => {
-    // Initialize Quill editor
-    const quill = new Quill(quillRef.current, {
-      theme: 'snow', // Theme of the editor
-      modules: {
-        toolbar: [
-          [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-          [{ 'align': [] }],
-          ['bold', 'italic', 'underline'],
-          ['link'],
-          ['image']
-        ],
-      },
-    });
-
