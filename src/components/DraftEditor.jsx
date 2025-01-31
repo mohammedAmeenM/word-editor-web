83a81d8996c214c88209aca17d25b92527b5f9a1
amiiiiee <amiameen507@gmail.com>
2025-01-31 17:30:51 +0530

    edits

--- src/components/DraftEditor.jsx
+++ src/components/DraftEditor.jsx
@@ -9,21 +9,3 @@
 //     setEditorState(state);
 //   };
 
-//   const handleKeyCommand = (command) => {
-//     const newState = RichUtils.handleKeyCommand(editorState, command);
-//     if (newState) {
-//       setEditorState(newState);
-//       return 'handled';
-//     }
-//     return 'not-handled';
-//   };
-
-//   const toggleInlineStyle = (inlineStyle) => {
-//     setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
-//   };
-
-//   return (
-//     <div className="p-4">
-//       <h1 className="text-2xl font-semibold text-black text-opacity-50 mb-4">
-//         Draft.js Word Editor
-//       </h1>
