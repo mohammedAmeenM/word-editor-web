faa8e15372662b6c233f06f5edc8888622d022bc
amiiiiee <amiameen507@gmail.com>
2025-01-31 17:29:24 +0530

    edits

--- src/components/SlateEditor.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useMemo, useCallback } from 'react';
-import { createEditor, Editor, Transforms, Text } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
-
-const SlateEditor = () => {
-    const [value, setValue] = useState([
-        {
