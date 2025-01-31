959d826ac0e4106392f431ef7b250ad1569ab493
amiiiiee <amiameen507@gmail.com>
2025-01-31 17:30:24 +0530

    edits

--- src/components/QuillEditor.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css'; // Import Quill's Snow theme CSS
-
-const QuillEditor = () => {
-  const quillRef = useRef(null);
-  const [editorContent, setEditorContent] = useState('');
