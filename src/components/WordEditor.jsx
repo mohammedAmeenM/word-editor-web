46631766b01423805b0634f5c95987d3c37bc342
amiiiiee <amiameen507@gmail.com>
2025-02-04 14:34:39 +0530

    edits

--- src/components/WordEditor.jsx
+++ src/components/WordEditor.jsx
@@ -379,10 +379,10 @@ const WordEditor = () => {
     <div className="flex flex-col ">
       {/* Sidebar Controls - Responsive positioning */}
       <div className="w-full  flex justify-center sticky top-5  z-50 ">
-        <div className="flex justify-center w-full lg:w-4/5 2xl:w-2/4  border border-opacity-30  p-3 gap-4 bg-gray-100 border-black rounded-xl">
+        <div className="flex justify-center w-full lg:w-4/5 xl:w-[52%] 2xl:w-[45%]  border border-opacity-30  py-7 px-9 gap-4 bg-gray-100 border-black rounded-xl">
           {/* Scene Action Button */}
 
-          <div className="flex flex-col w-1/4 space-y-4 ">
+          <div className="flex flex-col w-1/3 2xl:w-1/3 space-y-4 ">
             <button className={`border border-black border-opacity-30 rounded-full px-4 py-2 ${
             selectedButton === "Description" 
               ? "bg-pink-100 text-black font-semibold" 
@@ -391,7 +391,7 @@ const WordEditor = () => {
             <p  className="text-center text-opacity-70 text-black">Press Keyboard Shift+Tab</p>
           </div>
 {/* Add Character Button */}
-          <div className="flex flex-col w-1/4  space-y-4 ">
+          <div className="flex flex-col w-1/3 2xl:w-1/3  space-y-4 ">
             <button className={`border border-black border-opacity-30 rounded-full px-4 py-2 ${
             selectedButton === "Characters" 
               ? "bg-pink-100 text-black font-semibold" 
@@ -401,7 +401,7 @@ const WordEditor = () => {
           </div>
 
                    {/* Add Dialog Button */}
-          <div className="flex flex-col w-1/4  space-y-4 ">
+          <div className="flex flex-col w-1/3 2xl:w-1/3  space-y-4 ">
             <button className={`border border-black border-opacity-30 rounded-full px-4 py-2 ${
             selectedButton === "Dialog" 
               ? "bg-pink-100 text-black font-semibold" 
