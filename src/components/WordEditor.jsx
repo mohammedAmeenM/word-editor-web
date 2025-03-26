9fd29008d67ece168438c9d07fc247a2b4eda796
amiiiiee <amiameen507@gmail.com>
2025-03-26 12:32:06 +0530

    edits

--- src/components/WordEditor.jsx
+++ src/components/WordEditor.jsx
@@ -110,6 +110,8 @@ const WordEditor = () => {
       })
     );
   };
+
+  //onchange update contents-------------------------------------------------------------
   const handleContentChange = (sceneIndex, contentIndex, field, value) => {
     setScenes((prevScenes) =>
       prevScenes.map((scene, index) =>
