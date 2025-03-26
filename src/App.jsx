4bf72c03ea272dd0f69f45e22238ca76efb57418
amiiiiee <amiameen507@gmail.com>
2025-03-26 12:23:52 +0530

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import WordEditor from './components/WordEditor'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import WordEditor from "./components/WordEditor";
 
 function App() {
   return (
     <>
-    <Routes>
-
-      <Route path='/' element={<WordEditor />} />
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<WordEditor />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
